c48fc55988311b3e6d86cd50d2e146c1212ede65
rupeshchaudhari <rupeshachaudhari@gmail.com>
2020-10-07 17:37:25 +0400

    Initial Commit

---
+++ src/MusabC++.cpp
@@ -0,0 +1,37 @@
+#include <iostream>  // Importing the header file for IO Operations
+using namespace std;
+
+int main() {
+
+	bool condition = true;
+	condition = 1;
+	int num = 10;
+	long longNumber = 123879432;
+	float amount = 10.5;
+	double bigAmount = 23423809345.454;
+	char ch = 'A';
+	string name;
+
+	int a= 10;
+	int b= 20;
+	int c= a+b;
+
+	cout << "Sum of " << a  << " & " << b << " is " << c;
+
+	cout << "\nPlease enter your name: ";
+
+	cin >> name;
+
+	cout << "Hello " + name;
+
+	return 0;
+}
+
+
+// package
+
+// import
+
+
+// cout - Output
+// cin - Input
